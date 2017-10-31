ce3a64fe9cd0e13af7b5df0c1b128161de7c2b15
ndzwill <ndzwill@tapestrysolutions.com>
2017-10-30 21:05:20 -0700

    Pass all class names to get all rules when initializer is constructed.

--- jaffa-rules/source/java/org/jaffa/rules/initializers/RuleInitializerFactory.java
+++ jaffa-rules/source/java/org/jaffa/rules/initializers/RuleInitializerFactory.java
@@ -4,10 +4,14 @@ import org.apache.log4j.Logger;
 import org.jaffa.beans.factory.InitializerFactory;
 import org.jaffa.rules.RuleActorFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A factory for object initializers that are based on meta-data rules.
  */
 public class RuleInitializerFactory extends RuleActorFactory<Initializer> implements InitializerFactory {
+
     private static final Logger logger = Logger.getLogger(RuleInitializerFactory.class);
 
     /**
@@ -25,10 +29,27 @@ public class RuleInitializerFactory extends RuleActorFactory<Initializer> implem
     protected Initializer createActor(Object object) {
         FieldInitializer initializer = new FieldInitializer();
         try {
-            initializer.initializeRuleMapFromMetaData(object.getClass().getName());
+            initializer.initializeRuleMapFromMetaData(getClassNameList(object));
         } catch (Exception e) {
             logger.error("Exception while creating an initializer for " + object.getClass().getSimpleName(), e);
         }
         return initializer;
+    }
+
+    /**
+     * Gets a list of all class names in the hierarchy from the input class up to but not including Object.
+     * The list is ordered so subclasses come after their immediate super.
+     */
+    private List<String> getClassNameList(Object object) {
+        List<String> classNames = new ArrayList<>();
+        classNames.add(object.getClass().getName());
+        Class superclass = object.getClass().getSuperclass();
+
+        // stop iterating when we hit object
+        while ((superclass != null) && (superclass != Object.class)) {
+            classNames.add(0, superclass.getName());
+            superclass = superclass.getSuperclass();
+        }
+        return classNames;
     }
 }
