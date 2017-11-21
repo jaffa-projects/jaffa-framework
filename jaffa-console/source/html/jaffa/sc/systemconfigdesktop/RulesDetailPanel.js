f0747de114216b475ef0fbdf067227216cac69ca
stuta <stuta@tapestrysolutions.com>
2017-11-21 12:06:24 -0800

    GOLD-67391 - Fix for systemconfigdesktop to be read-only : Merge Request by Matt
    Signed-off-by: stuta <stuta@tapestrysolutions.com>

--- jaffa-console/source/html/jaffa/sc/systemconfigdesktop/RulesDetailPanel.js
+++ jaffa-console/source/html/jaffa/sc/systemconfigdesktop/RulesDetailPanel.js
@@ -37,67 +37,30 @@ Jaffa.SC.RulesDetailPanel = Ext.extend(Ext.grid.EditorGridPanel, {
       )
     });
 
-    this.ruleRenderer =  function(value, metadata, record, rowIndex, colIndex, store){
-      var meta;
-      if (record && record.get('metaData')){
-        meta = record.get('metaData');
-      } else {
-        return value;
-      }
-
-      if (meta.inList){
-        var valueList = value.split(',');
-        var renderedList = [];
-        for (var i = 0; i < valueList.length; i++){
-          for (var j = 0; j < meta.inList.length; j++){
-            if (meta.inList[j][0] == valueList[i])
-              renderedList[renderedList.length] = meta.inList[j][1];
-          }
-        }
-        value = renderedList.join(', ');
-      }
-
-      if (!meta.readOnly && security.hasRuleMaintenance)
-        metadata.css = 'editable';
-
-      return value;
-    };
-    
-
-    var tbar = [
-      {
-        iconCls:'save',
-        text:Labels.get('label.Jaffa.SC.SystemConfigDesktop.save'),
-        itemId: 'save',
-        disabled: !this.controller.isDirty(),
-        handler:function(){
-          Jaffa.SC.DesktopController.save();
-        }
-      }
-    ];
+    var tbar;
     if (this.enableSearch)
-      tbar = tbar.concat([
-        '-',
-        {xtype:'textfield', emptyText:Labels.get('label.Jaffa.SC.SystemConfigDesktop.searchValue'),ref:'searchField',
-          listeners : {
+      tbar = [
+          '-',
+          {xtype:'textfield', emptyText:Labels.get('label.Jaffa.SC.SystemConfigDesktop.searchValue'),ref:'searchField',
+            listeners : {
             "specialkey": function(field, ev){
-              if(ev.getKey() == ev.ENTER){
-                ev.preventDefault();
-                this.ownerCt.ownerCt.search(this.getValue());
-              }
-            },
-            keydown: {
-              fn: function(){
-                this.ownerCt.ownerCt.search(this.getValue());
+                if(ev.getKey() == ev.ENTER){
+                    ev.preventDefault();
+                    this.ownerCt.ownerCt.search(this.getValue());
+                  }
               },
-              buffer: 350
-            }
+            keydown: {
+                fn: function(){
+                    this.ownerCt.ownerCt.search(this.getValue());
+                  },
+                buffer: 350
+              }
           }
-        },{
+      },{
           itemId:'searchCount',
-          text: Labels.get('label.Jaffa.Widgets.Button.Search') + ': 0 '+Labels.get('label.Common.of')+' 0'
+              text: Labels.get('label.Jaffa.Widgets.Button.Search') + ': 0 '+Labels.get('label.Common.of')+' 0'
         }
-      ]);
+    ];
 
     Ext.apply(this, {
       title: this.path?this.path:Labels.get('label.Jaffa.SC.SystemConfigDesktop.mainTabTitle'),
@@ -137,13 +100,11 @@ Jaffa.SC.RulesDetailPanel = Ext.extend(Ext.grid.EditorGridPanel, {
         dataIndex: 'varRule',
         width : 200,
         sortable: true,
-        renderer: this.ruleRenderer
       },{
         header: Labels.get('label.Jaffa.SC.SystemConfigDesktop.global'),
         dataIndex: 'globalRule',
         width : 200,
         sortable: true,
-        renderer: this.ruleRenderer
       }],
       ds: new Ext.data.Store({
         scope: this,
@@ -154,54 +115,8 @@ Jaffa.SC.RulesDetailPanel = Ext.extend(Ext.grid.EditorGridPanel, {
           direction: "ASC"
         },
         reader: new Ext.data.JsonReader({idProperty : 'rule'}, this.ruleRecord)
-      }),
-      listeners:{
-        'cellclick': {
-          fn: function (grid, rowIndex, columnIndex, e){
-            if (grid.activeEditor || !security.hasRuleMaintenance) return false;
-            if (grid.getColumnModel().getColumnAt(columnIndex).dataIndex!='varRule'&&grid.getColumnModel().getColumnAt(columnIndex).dataIndex != 'globalRule') return;
-            var record = grid.getStore().getAt(rowIndex);
-            var _editor = "";
-            if (record.get('metaData')&&record.get('metaData').readOnly===true) return false;
-            var fieldConfig = this.buildEditor(record);
-            _editor = new Ext.grid.GridEditor(Ext.ComponentMgr.create(fieldConfig));
-			if(!Ext.isEmpty(fieldConfig.foreignKeyInfo)){
-			  _editor.on('beforestartedit', function(editor, el, v){
-			    this.field.setGridFilters = function(){};
-			    this.startValue = v;
-			    this.field.setValue(v);
-			    this.field.setRawValue(v);
-			    this.field.initialized = true;
-			    this.realign(true);
-			    this.editing = true;
-			    this.show();
-			    return false;
-		      });	
-			}
-            grid.getColumnModel().setEditor(columnIndex,_editor);
-          }
-        },
-        beforeClose: function(){
-          if (this._dirty){
-            Ext.MessageBox.show({
-              title : Labels.get('label.Jaffa.SC.SystemConfigDesktop.SaveWarning'),
-              msg : Labels.get('label.Jaffa.SC.SystemConfigDesktop.SaveWarningMessage'),
-              width : 400,
-              buttons : Ext.MessageBox.YESNO,
-              icon : Ext.MessageBox.QUESTION,
-              scope: this,
-              fn : function(btn) {
-                if (btn == 'yes') {
-                  this.setDirty(false);
-                  this.ownerCt.remove(this);
-                }
-              }
-            });
-            return false;
-          }
-        }
-      }
-    })
+      })
+  })
 
     Ext.grid.EditorGridPanel.superclass.initComponent.call(this);
 
@@ -352,104 +267,35 @@ Jaffa.SC.RulesDetailPanel = Ext.extend(Ext.grid.EditorGridPanel, {
           return ClassMetaData.BusinessRules.fields[p.join('.')];
       }
 
-      var p = property.split('.');
-      p = p[p.length-1];
-      if (ClassMetaData.BusinessRules.fields['**.' + p])
-        return ClassMetaData.BusinessRules.fields['**.' + p];
-    }
-    return null;
-  },
-  buildEditor: function(record){
-	var meta = record.get('metaData'),
-	  config = {};	
-	  config.xtype = 'textfield';
-	
-    if (meta){
-      switch (meta.type) {
-        case 'string':
-          if (meta.inList){
-            config.xtype = 'combo';
-            config.forceSelection = true;
-		  } else if(meta.foreignKeyInfo){
-		    config.xtype = 'finderLovComboGrid';
-	  		config.foreignKeyInfo = meta.foreignKeyInfo;
-		    config.popupAlways = true;
-          } else {
-            config.xtype = 'textfield';
-            if (meta.maxLength) config.maxLength=meta.maxLength;
-            if (meta.minLength) config.minLength=meta.minLength;
-            if (meta.caseType) config.style = 'text-transform: uppercase;';
-          }
-          break;
-        case 'number':
-          config.xtype = 'numberfield';
-          if (meta.maxValue) config.maxValue=meta.maxValue;
-          if (meta.minValue) config.minValue=meta.minValue;
-          break;
-        case 'dateonly':
-          config.xtype = 'datefield';
-          break;
-        case 'datetime':
-          config.xtype = 'xdatetime';
-          break;
-        case 'boolean':
-          config.xtype = 'combo';
-          config.mode='local';
-          config.triggerAction='all';
-          config.forceSelection=true;
-          config.store = new Ext.data.ArrayStore({
-            id: 0,
-            fields: ['valueText'],
-            data: [['""'], ['true'], ['false']]
-          });
-          config.valueField='valueText';
-          config.displayField='valueText';
-          break;
-        case 'object':
-          if (meta.inList){
-            config.xtype = 'lovcombo';
-            config.displayField = 'field2';
-            config.valueField = 'field1';
-          }else
-            config.xtype = 'textfield';
-          break;
-        default:
-          config.xtype = 'textfield';
-      }
-    }
-    if (meta && meta.inList){
-      config.store = meta.inList;
-      config.minChars= 0;
-      config.triggerAction= 'all';
-      config.initialized= 'all';
-      config.maxHeight= 150;
-      config.resizable= true;
-    }
-
-    return config;
-  },
-  _dirty: false,
-  setDirty: function(dirty) {
-    if (dirty && !this._dirty){
-      this.setTitle(this.baseTitle + '*');
-      this._dirty = true;
-    }
-    if (!dirty) {
-      this.store.commitChanges();
-      this._dirty = false;
-      this.setTitle(this.baseTitle);
-    }
-  },
-  refresh: function(){
-    this.setDirty(false);
-    if (this.path){
-      this.loadFromPath(this.path);
-    }else if (this.searchString){
-      this.search(this.searchString);
-    }
-    if (this.store.sortInfo && this.store.sortInfo.field && this.store.sortInfo.direction){
-      this.store.sort(this.store.sortInfo.field,this.store.sortInfo.direction);
-    }
+    var p = property.split('.');
+    p = p[p.length-1];
+    if (ClassMetaData.BusinessRules.fields['**.' + p])
+      return ClassMetaData.BusinessRules.fields['**.' + p];
   }
+  return null;
+},
+_dirty: false,
+    setDirty: function(dirty) {
+  if (dirty && !this._dirty){
+    this.setTitle(this.baseTitle + '*');
+    this._dirty = true;
+  }
+  if (!dirty) {
+    this.store.commitChanges();
+    this._dirty = false;
+    this.setTitle(this.baseTitle);
+  }
+},
+refresh: function(){
+  this.setDirty(false);
+  if (this.path){
+    this.loadFromPath(this.path);
+  }else if (this.searchString){
+    this.search(this.searchString);
+  }
+  if (this.store.sortInfo && this.store.sortInfo.field && this.store.sortInfo.direction){
+    this.store.sort(this.store.sortInfo.field,this.store.sortInfo.direction);
+  }
+}
 
 });
