9ea64e0ce94474a9779a19b3db8cfb1cc75b6f29
stuta <stuta@tapestrysolutions.com>
2018-05-30 22:15:33 -0700

    Initialize lifecyclehandlers on clone of domain object

--- jaffa-core/source/java/org/jaffa/persistence/Persistent.java
+++ jaffa-core/source/java/org/jaffa/persistence/Persistent.java
@@ -168,9 +168,9 @@ public abstract class Persistent implements IPersistent, IFlexFields {
 
         // Initialize the fields
         obj.m_uow = null;
-        obj.lifecycleHandlers = null;
-        obj.appendedHandlers = null;
-        obj.prependedHandlers = null;
+        obj.lifecycleHandlers = new ArrayList<>();
+        obj.appendedHandlers = new ArrayList<>();
+        obj.prependedHandlers = new ArrayList<>();
         //obj.m_modified = false;
         obj.m_databaseOccurence = false;
         obj.m_locking = Criteria.LOCKING_OPTIMISTIC;
