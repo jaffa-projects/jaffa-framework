e5bc0e2f09006ae7c1b1ca725ab345b9ed8ec43c
stuta <stuta@tapestrysolutions.com>
2019-08-06 15:03:16 -0700

    JAFFA-651 Supporting email address as printer MUTT commands are not included in v17 docker containers. Check-in on behalf of Raghu

--- jaffa-components-printing/source/java/org/jaffa/modules/printing/services/FormDelivery.java
+++ jaffa-components-printing/source/java/org/jaffa/modules/printing/services/FormDelivery.java
@@ -56,6 +56,7 @@ import org.jaffa.modules.printing.services.exceptions.EmailFailedException;
 import org.jaffa.modules.printing.services.exceptions.PrintFailedException;
 import org.jaffa.persistence.UOW;
 import java.io.File;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.mail.MessagingException;
 import org.apache.log4j.Logger;
@@ -72,71 +73,93 @@ import org.jaffa.util.StringHelper;
 
 public class FormDelivery {
 
-    /** Logger reference */
+    /**
+     * Logger reference
+     */
     private final static Logger log = Logger.getLogger(FormDelivery.class);
-    
+
     /**
      * This will deliver a file based on the delivery options in the PrintRequest
-     * @param request The print request detailing how this should be delivered
+     *
+     * @param request  The print request detailing how this should be delivered
      * @param document The generated file that should be delivered
-     * @param context A context object that can be used to extract values when generating e-mail
-     * subject and body information. This object should be a java bean or a map
+     * @param context  A context object that can be used to extract values when generating e-mail
+     *                 subject and body information. This object should be a java bean or a map
      * @throws ApplicationExceptions Thrown if any funtional issue ocurred when executing
-     * @throws FrameworkException Thrown if any framework issue ocurred when executing
+     * @throws FrameworkException    Thrown if any framework issue ocurred when executing
      */
     public static void deliver(PrintRequest request, File document, Object context)
-        throws FrameworkException, ApplicationExceptions
-    {
+            throws FrameworkException, ApplicationExceptions {
         // See if it should be printed
-        if(request.getPrinterId()!=null && request.getPrintCopies()!=null && request.getPrintCopies().intValue() > 0)
-            sendToPrinter(request.getPrinterId(),request.getPrintCopies().intValue(), document);
-        
+        PrinterDefinition printer = null;
+        if (request.getPrinterId() != null && request.getPrintCopies() != null && request.getPrintCopies().intValue() > 0)
+            sendToPrinter(request.getPrinterId(), request.getPrintCopies().intValue(), document);
+
         // See if it should be e-mailed
-        if (request.getEmailToAddresses() != null)
+        if (request.getPrinterId() != null) {
+            printer = PrinterDefinition.findByPK(null, request.getPrinterId());
+        }
+        if (request.getEmailToAddresses() != null) {
             sendEmail(request, document, context);
+        } else if (printer != null) {
+            sendToEmailPrinter(request, printer, document, context);
+        }
     }
-    
-    
+
+
+    private static void sendToEmailPrinter(PrintRequest request, PrinterDefinition printer, File document, Object context) throws FrameworkException, ApplicationExceptions {
+        String regex = "^(.+)@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+
+        if (printer != null && printer.getRealPrinterName() != null) {
+            Matcher matcher = pattern.matcher(printer.getRealPrinterName());
+            if (matcher != null && matcher.matches()) {
+                request.setEmailToAddresses(printer.getRealPrinterName());
+                sendEmail(request, document, context);
+            }
+        }
+    }
+
     /**
      * Send the generated document to the specified printer
+     *
      * @param printerId Printer name, should be defined in PrinterDefinitions
-     * @param copies Number of copies to print
-     * @param document Source file to print
+     * @param copies    Number of copies to print
+     * @param document  Source file to print
      * @throws ApplicationExceptions Thrown if any funtional issue ocurred when executing
-     * @throws FrameworkException Thrown if any framework issue ocurred when executing
+     * @throws FrameworkException    Thrown if any framework issue ocurred when executing
      */
     public static void sendToPrinter(String printerId, int copies, File document)
-        throws FrameworkException, ApplicationExceptions
-    {
+            throws FrameworkException, ApplicationExceptions {
         UOW uow = new UOW();
         try {
 
             // Read the printer definition
-            PrinterDefinition printer = PrinterDefinition.findByPK(uow,printerId);
-            if( printer == null ) {
+            PrinterDefinition printer = PrinterDefinition.findByPK(uow, printerId);
+            if (printer == null) {
                 log.error("Unknown printer in print request : " + printerId);
                 throw new ApplicationExceptions(new DomainObjectNotFoundException(PrinterDefinitionMeta.getLabelToken()));
             }
             PrinterOutputType outputType = printer.getPrinterOutputTypeObject();
 
             // See if this is direct printing
-            if(Boolean.TRUE.equals(outputType.getDirectPrinting())) {
+            if (Boolean.TRUE.equals(outputType.getDirectPrinting())) {
                 printDirect(printer, document, copies);
             } else {
                 // Find an external command line that will print this document
                 String commandLine = null;
                 String os = System.getProperty("os.name");
 
-                for(OutputCommand command : outputType.getOutputCommandArray()) {
-                    if(Pattern.matches(command.getOsPattern(), os)) {
-                        commandLine=command.getCommandLine();
+                for (OutputCommand command : outputType.getOutputCommandArray()) {
+                    if (Pattern.matches(command.getOsPattern(), os)) {
+                        commandLine = command.getCommandLine();
                         break;
                     }
                 }
 
-                if(commandLine==null) {
-                    log.error("Can't find matching command line for Output Type " + outputType.getOutputType() +" on OS " + os);
-                   throw new ApplicationExceptions(new DomainObjectNotFoundException(OutputCommandMeta.getLabelToken()) );
+                if (commandLine == null) {
+                    log.error("Can't find matching command line for Output Type " + outputType.getOutputType() + " on OS " + os);
+                    throw new ApplicationExceptions(new DomainObjectNotFoundException(OutputCommandMeta.getLabelToken()));
                 }
 
                 // Now try and print the document with the command
@@ -148,7 +171,7 @@ public class FormDelivery {
             aes.add(e);
             throw aes;
         } finally {
-            if(uow!=null)
+            if (uow != null)
                 uow.rollback(); // This UOW should have not been used for updates!!!
         }
     }
@@ -156,15 +179,16 @@ public class FormDelivery {
 
     /**
      * Email out the generated document as an attachment
-     * @param request Print Request containing possible email info
+     *
+     * @param request  Print Request containing possible email info
      * @param document document to put as the email attachment
-     * @param dom A context object that can be used to extract values when generating e-mail
-     * subject and body information. This object should be a java bean or a map
+     * @param dom      A context object that can be used to extract values when generating e-mail
+     *                 subject and body information. This object should be a java bean or a map
      * @throws ApplicationExceptions Thrown if any funtional issue ocurred when executing
-     * @throws FrameworkException Thrown if any framework issue ocurred when executing
+     * @throws FrameworkException    Thrown if any framework issue ocurred when executing
      */
     public static void sendEmail(PrintRequest request, File document, Object dom)
-    throws FrameworkException, ApplicationExceptions {
+            throws FrameworkException, ApplicationExceptions {
 
         String documentName = null;
 
@@ -172,13 +196,13 @@ public class FormDelivery {
             // Set up parameters
             Object[] params = null;
 
-            if(request instanceof FormPrintRequest) {
-                FormPrintRequest fpr = (FormPrintRequest)request;
-                
-                params = new Object[] {
-                    fpr.getFormName(),
-                    fpr.getFormAlternateName(),
-                    fpr.getVariation()
+            if (request instanceof FormPrintRequest) {
+                FormPrintRequest fpr = (FormPrintRequest) request;
+
+                params = new Object[]{
+                        fpr.getFormName(),
+                        fpr.getFormAlternateName(),
+                        fpr.getVariation()
                 };
                 documentName = fpr.getFormName();
             } else {
@@ -190,13 +214,13 @@ public class FormDelivery {
 
             // Use the resource bundle to get subject and body of this message
             String subject = request.getEmailSubject();
-            if(subject == null) {
-                String token = "label.Jaffa.Printing.Email."+documentName+".subject";
+            if (subject == null) {
+                String token = "label.Jaffa.Printing.Email." + documentName + ".subject";
                 subject = MessageHelper.findMessage(token, params);
             }
             String body = request.getEmailMessage();
-            if(body == null) {
-                String token = "label.Jaffa.Printing.Email."+documentName+".body";
+            if (body == null) {
+                String token = "label.Jaffa.Printing.Email." + documentName + ".body";
                 body = MessageHelper.findMessage(token, params);
             }
 
@@ -204,49 +228,48 @@ public class FormDelivery {
             //the subject and body for better messages. Expose request and dom to the template
 
             EmailerBean eb = new EmailerBean();
-            eb.sendMail(fromAddress, StringHelper.parseString(request.getEmailToAddresses(),";"),
-                    subject, body, new File[] {document});
+            eb.sendMail(fromAddress, StringHelper.parseString(request.getEmailToAddresses(), ";"),
+                    subject, body, new File[]{document});
 
         } catch (MessagingException e) {
             log.error("Failed to send email with attachment " + document.getAbsolutePath(), e);
             throw new ApplicationExceptions(
-                new EmailFailedException(documentName) );
+                    new EmailFailedException(documentName));
         }
     }
 
 
-
     private static void printDirect(PrinterDefinition printer, File document, int copies)
-        throws FrameworkException, ApplicationExceptions {
+            throws FrameworkException, ApplicationExceptions {
 
         // @TODO Use Printer APIs to and file to the printer
     }
 
-    /** Example command line: C:\gsview\gsview32.exe "-p{0}" {2} {3} {1}
+    /**
+     * Example command line: C:\gsview\gsview32.exe "-p{0}" {2} {3} {1}
      */
     private static void printViaCommand(PrinterDefinition printer, File document, int copies, String commandLine)
-        throws FrameworkException, ApplicationExceptions
-    {
+            throws FrameworkException, ApplicationExceptions {
         // Perform parameter substitution on the command line
         // {0} is the printer name
         // {1} is the filename to print
         // {2} is additional parameter 1
         // {3} is additional parameter 2
-        commandLine = StringHelper.replace(commandLine, "{0}", document.getAbsolutePath() );
-        commandLine = StringHelper.replace(commandLine, "{1}", printer.getRealPrinterName() );
-        commandLine = StringHelper.replace(commandLine, "{2}", printer.getPrinterOption1()==null?"":printer.getPrinterOption1() );
-        commandLine = StringHelper.replace(commandLine, "{3}", printer.getPrinterOption2()==null?"":printer.getPrinterOption2() );
+        commandLine = StringHelper.replace(commandLine, "{0}", document.getAbsolutePath());
+        commandLine = StringHelper.replace(commandLine, "{1}", printer.getRealPrinterName());
+        commandLine = StringHelper.replace(commandLine, "{2}", printer.getPrinterOption1() == null ? "" : printer.getPrinterOption1());
+        commandLine = StringHelper.replace(commandLine, "{3}", printer.getPrinterOption2() == null ? "" : printer.getPrinterOption2());
 
         if (log.isDebugEnabled()) {
             log.debug("Print using command line: " + commandLine);
         }
-        
+
         // Execute the command line using OsScriptingBean
         OsScriptingBean os = new OsScriptingBean();
-        os.setCommand(new String[] {commandLine});
-        for(int i =0; i<copies; i++) {
+        os.setCommand(new String[]{commandLine});
+        for (int i = 0; i < copies; i++) {
             try {
-                if(os.exec()<0) {
+                if (os.exec() < 0) {
                     // Handle OS error
                     log.error("Error on Command " + commandLine);
                     log.info("Standard Output:\n" + os.getOutput());
